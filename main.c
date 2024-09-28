12c04a75b4536b764ea9fcd4787342024495445f
hamshad <hiromega18@gmail.com>
2024-09-28 18:05:59 +0530

    added addStudent and viewStudent

--- main.c
+++ main.c
@@ -55,9 +55,66 @@ int main() {
         printf("Exiting the program, Goodbye!\n");
         exit(EXIT_FAILURE);
       default:
-        printf(c"Invalid choice! Please enter a number between 1 and 6.\n");
+        printf("Invalid choice! Please enter a number between 1 and 6.\n");
     }
   }
 
   return 0;
 }
+
+
+void addStudent() {
+  FILE *fp;   // File pointer for file operations
+  Student s;  // Variable of type Student to hold the students's information
+  
+
+  // Open the file in append mode (to add data without overwriting)
+  fp = fopen(FILE_NAME, "ab");
+  if (fp == NULL) {
+    printf("Error opening file!\n");
+    return;
+  }
+
+
+  // Collect student details from the user
+  printf("\n=== Add New Student ===\n");
+  printf("Enter ID: ");
+  scanf("%d", &s.id);
+  printf("Enter Name: ");
+  scanf("%[^\n]", &s.name);
+  printf("Enter Age: ");
+  scanf("%d", &s.age);
+  printf("Enter Grade: ");
+  scanf("%[^\n]", &s.grade);
+
+
+  fwrite(&s, sizeof(Student), 1, fp);
+  fclose(fp);
+
+  printf(c"Student added successfully!\n");
+}
+
+
+void viewStudents() {
+  FILE *fp;
+  Student s;
+
+  // Open the file in read mode
+  fp = fopen(FILE_NAME, "rb");
+  if (fp == NULL) {
+    printf("No student records found.\n");
+    return;
+  }
+
+  printf("\n--- List of Students ---\n");
+  printf("ID\tName\t\tAge\tGrade\n"):
+  printf("----------------------------------------\n");
+
+  // Read and display student's details one by one from the file
+  while(fread(&s, sizeof(Student), 1, fp)) {
+    printf("%d\t-15s\t%d\t%s\n", s.id, s.name, s.age, s.grade);
+  }
+
+  fclose(fp);
+}
+
